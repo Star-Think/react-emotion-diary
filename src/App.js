05dc431bd02db592fee531b4ddecd5776764f0f0
kimjonghyo <qaws2921@hanmail.net>
2022-07-06 10:26:46 +0900

    일기 세부 보기

--- src/App.js
+++ src/App.js
@@ -72,7 +72,7 @@ const dummyData = [
 function App() {
   const [data, dispatch] = useReducer(reducer, dummyData);
 
-  const dataId = useRef(0);
+  const dataId = useRef(6);
   // CREATE
   const onCreate = (date, content, emotion) => {
     dispatch({
@@ -110,7 +110,7 @@ function App() {
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/new" element={<New />} />
-              <Route path="/edit" element={<Edit />} />
+              <Route path="/edit/:id" element={<Edit />} />
               <Route path="/diary/:id" element={<Diary />} />
             </Routes>
           </div>
--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,14 +1,37 @@
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { useNavigate, useParams, useSearchParams } from "react-router-dom";
+import { DiaryStateContext } from "../App";
+import DiaryEditor from "../components/DiaryEditor";
 
 const Edit = () => {
-  const navgiate = useNavigate();
-  const [searchParams, setSearchParams] = useSearchParams();
+  const [originData, setOriginData] = useState();
 
-  const id = searchParams.get("id");
+  const navgiate = useNavigate();
+  // const [searchParams, setSearchParams] = useSearchParams();
+
+  // const id = searchParams.get("id");
+
+  const { id } = useParams();
+
+  const diaryList = useContext(DiaryStateContext);
+
+  useEffect(() => {
+    if (diaryList.length > 1) {
+      const targetDiary = diaryList.find(
+        (it) => parseInt(it.id) === parseInt(id)
+      );
+
+      if (targetDiary) {
+        setOriginData(targetDiary);
+      } else {
+        navgiate("/", { replace: true });
+      }
+    }
+  }, [id, diaryList]);
 
   return (
     <div>
-      <h1>Edit</h1>
+      {/* <h1>Edit</h1>
       <p>이곳은 일기 수정페이지 입니다.</p>
       <button onClick={() => setSearchParams({ who: "starthink" })}>
         QS 바꾸기
@@ -20,7 +43,9 @@ const Edit = () => {
         }}
       >
         뒤로가기
-      </button>
+      </button> */}
+      {originData && <DiaryEditor isEdit={true} originData={originData} />}
+      <h2>Edit</h2>
     </div>
   );
 };
